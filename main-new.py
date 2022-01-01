7293ddeb11fbd66c3d5fe2f66a974e5add450323
pformela_ <p.formela.030@studms.ug.edu.pl>
2022-01-01 15:13:32 +0100

    finished game

--- main-new.py
+++ main-new.py
@@ -2,6 +2,7 @@ import pygame
 import os
 
 pygame.font.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -13,7 +14,11 @@ BULLET_VELOCITY = 8
 MAX_BULLETS = 3
 BORDER = pygame.Rect((WIDTH-10)/2, 0, 10, HEIGHT)
 
+# BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('sounds', 'name1.mp3'))
+# BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('sounds', 'name2.mp3'))
+
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100) 
 
 PATH = 'images'
 
@@ -22,6 +27,13 @@ SECOND_SPACESHIP = pygame.transform.rotate(pygame.image.load(os.path.join(PATH, 
 
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
+
+
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text, 1, (0,0,0))
+    WIN.blit(draw_text, ((WIDTH//2 - draw_text.get_width()//2), (HEIGHT//2 - draw_text.get_height()//2)))
+    pygame.display.update()
+    pygame.time.delay(5000)
 
 
 def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
@@ -107,19 +119,24 @@ def main() :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(red_bullets) < MAX_BULLETS:
                     red_bullet = pygame.Rect(red.right, red.centery, 10, 5)
                     red_bullets.append(red_bullet)
+                    # BULLET_FIRE_SOUND.play()
                 if event.key == pygame.K_RCTRL and len(yellow_bullets) < MAX_BULLETS:
                     yellow_bullet = pygame.Rect(yellow.left, yellow.centery, 10, 5)
                     yellow_bullets.append(yellow_bullet)
+                    # BULLET_FIRE_SOUND.play()
 
             if event.type == RED_HIT:
                 red_health -= 1
+                # BULLET_HIT_SOUND.play()
 
             if event.type == YELLOW_HIT:
                 yellow_health -= 1
+                # BULLET_HIT_SOUND.play()
 
         if red_health <= 0:
             winner_text = "Yellow wins"
@@ -128,7 +145,8 @@ def main() :
             winner_text = "Red wins"
 
         if winner_text != "":
-            pass
+            draw_winner(winner_text)
+            break
 
 
         keys_pressed = pygame.key.get_pressed()
@@ -139,7 +157,7 @@ def main() :
 
         draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
 
-    pygame.quit()
+    main()
 
 
 if __name__ == "__main__":
